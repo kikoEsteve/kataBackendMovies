9f3803158d8fcd235e0487d6e95ed9e45d5984b0
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-12-08 15:46:53 +0100

    middleware/cors.js updated

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,3 +4,8 @@ export default (req,res,next) => {
     res.header('Access-Controll-Allow-Methods', '*');
     next();
 }
+
+// Cross-Origin Resorce Sharing
+// HTTP-header based mechanism that allows a server 
+// to indicate any other origins (domain, protocol, or port) 
+// than its own from which a browser should permit loading of resources.
