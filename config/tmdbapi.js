f9f351a15aa1efa57ab8b5985740fe866876aaa7
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-12-08 15:45:42 +0100

    config/tmdbapi.js updated

--- config/tmdbapi.js
+++ config/tmdbapi.js
@@ -3,14 +3,14 @@ import './mongoose.js';
 import MovieModel from '../models/Movie.js';
 
 const seedMovies = async () => {
-    const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES');
+    const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=2f0ee4659e75f22b0645bb580847d157&language=es-ES');
     let {
         total_pages,
         results
     } = res.data;
     await MovieModel.insertMany(results);
     for (let i = 2; i <= total_pages; i++){
-        const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&page=' + i);
+        const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=2f0ee4659e75f22b0645bb580847d157&language=es-ES&page=' + i);
         await MovieModel.insertMany(response.data.results);
     }
 }
