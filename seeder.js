6d7dc9bc3135528b6169ceeb18314ec0f8416c21
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-12-08 15:47:22 +0100

    seeder.js updated

--- seeder.js
+++ seeder.js
@@ -10,3 +10,7 @@ const connection = await mysql.createConnection({
 const [movies, fields] = await connection.execute('SELECT * FROM movies');
 await MovieModel.insertMany(movies);
 console.log('Data Base Seeded');
+
+// Suppose we want to insert some data into a few tables by default. 
+// To manage all data migrations you can use seeders. 
+// Seed files are some change in data that can be used to populate database table with sample data or test data.
